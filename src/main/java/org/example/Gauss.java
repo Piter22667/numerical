c80cc299759e774cf4327ea15c790d7fc3a04103
Piter Bilyk <bilykpiter492@gmail.com>
2024-11-02 00:24:40 +0200

    matrix and vector for Gauss method

--- src/main/java/org/example/Gauss.java
+++ src/main/java/org/example/Gauss.java
@@ -2,6 +2,21 @@ package org.example;
 
 public class Gauss {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int n = 4;
+        double[][] A = {
+                {7, 2, 3, 0},
+                {0, 3, 2, 6},
+                {2, 5, 1, 0},
+                {0, 1, 4, 2}
+        };
+        double[] b = {20, 36, 15, 22};
+        double[] x = new double[n];
+        System.out.println("Matrix A:");
+        //
+
+        System.out.println("\nVector b:");
+        for (int i = 0; i < n; i++) {
+            System.out.printf("%.2f\n", b[i]);
+        }
     }
 }
